ac90a8105b2ea48164dd64121123942a04df2a5e
jesswinw <jesswinw@gmail.com>
2020-05-25 13:09:51 +0530

    burn all task feature added 🔥

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //selcetors
 const todoInput = document.querySelector('.todo-input')
 const todoBtn = document.querySelector('.todo-btn')
+const burnBtn = document.querySelector('.burn-btn')
 const todoList = document.querySelector('.todo-list')
 
 //listeners
 todoBtn.addEventListener('click', event => {
     event.preventDefault()
     addtask()
+})
+
+burnBtn.addEventListener('click', event => {
+    event.preventDefault()
+    burn()
 })
 
 //fns
@@ -18,7 +24,11 @@ const addtask = () => {
     //list
     const list = document.createElement('li')
     list.classList.add('todo-item')
-    list.innerText = 'hello'
+    if (todoInput.value == '') {
+        return 0
+    }
+    list.innerText = todoInput.value
+    todoInput.value = ''
     taskDiv.appendChild(list)
 
     //check
@@ -36,4 +46,9 @@ const addtask = () => {
 
     //adding to the html
     todoList.appendChild(taskDiv)
+}
+
+const burn = () => {
+    const items = document.querySelectorAll('.task')
+    items.forEach(item => item.remove())
 }
