c61ca7dd2e0d5d78db006a945357897765a69f3e
jesswinw <jesswinw@gmail.com>
2020-05-25 18:00:58 +0530

    app now uses localstorage to save tasks😀

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const burnBtn = document.querySelector(".burn-btn");
 const todoList = document.querySelector(".todo-list");
 
 //listeners
+document.addEventListener("DOMContentLoaded", () => getTasks());
 todoBtn.addEventListener("click", (event) => {
   event.preventDefault();
   addtask();
@@ -36,7 +37,9 @@ const addtask = () => {
   if (todoInput.value == "") {
     return 0;
   }
-  list.innerText = todoInput.value;
+  const task = todoInput.value;
+  addTostorage(task);
+  list.innerText = task;
   todoInput.value = "";
   taskDiv.appendChild(list);
 
@@ -59,17 +62,90 @@ const addtask = () => {
   todoList.appendChild(taskDiv);
 };
 
+// Clearing all the tasks
 const burn = () => {
   const items = document.querySelectorAll(".task");
   items.forEach((item) => {
     item.classList.add("delprep");
     item.addEventListener("transitionend", () => item.remove());
   });
+  localStorage.setItem("tasks", JSON.stringify([]));
 };
 
 const delTask = (task) => {
   task.classList.add("delprep");
+  delfrmStorage(task.innerText);
   task.addEventListener("transitionend", () => task.remove());
 };
 
 const checkTask = (task) => task.classList.toggle("complete");
+
+// Adding each task to the storage
+const addTostorage = (task) => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Display saved Tasks
+const getTasks = () => {
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    return 0;
+  }
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  tasks.forEach((task) => {
+    // list item
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+
+    //list
+    const list = document.createElement("li");
+    list.classList.add("todo-item");
+    list.innerText = task;
+    todoInput.value = "";
+    taskDiv.appendChild(list);
+
+    //check
+
+    const checkBtn = document.createElement("button");
+    checkBtn.classList.add("check");
+    checkBtn.classList.add("btn");
+    checkBtn.innerHTML = '<i class="fas fa-check"></i>';
+    taskDiv.appendChild(checkBtn);
+
+    //trash
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trash");
+    trashBtn.classList.add("btn");
+    trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    taskDiv.appendChild(trashBtn);
+
+    //adding to the html
+    todoList.appendChild(taskDiv);
+  });
+};
+
+// Deleting Tasks
+const delfrmStorage = (task) => {
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    return 0;
+  }
+
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  const index = tasks.indexOf(task);
+
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
