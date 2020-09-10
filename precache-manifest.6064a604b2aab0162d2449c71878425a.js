8e0850771950d24cee4540f5a32c51f62e32b288
jesswinw <jesswinw@gmail.com>
2020-09-10 16:31:25 +0530

    Updates

---
+++ precache-manifest.6064a604b2aab0162d2449c71878425a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9f870bf0e9e615df277d7256eb7ef7",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "031e83bbf6422543eaca",
+    "url": "/ToDo-App/static/css/main.9b23b0af.chunk.css"
+  },
+  {
+    "revision": "60b2c25256c17874a803",
+    "url": "/ToDo-App/static/js/2.7f5be3f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App/static/js/2.7f5be3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031e83bbf6422543eaca",
+    "url": "/ToDo-App/static/js/main.ac12605a.chunk.js"
+  },
+  {
+    "revision": "c154b869f5e9501d30c6",
+    "url": "/ToDo-App/static/js/runtime-main.139b2881.js"
+  },
+  {
+    "revision": "baa7805e92a7c940a7783e76c5e308d6",
+    "url": "/ToDo-App/static/media/bg-confetti.baa7805e.png"
+  }
+]);
